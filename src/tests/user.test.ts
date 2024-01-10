dc70f0e57c0260194c029131dcedb2738a5fd415
Roland Saavedra <rolandsaavedra22@gmail.com>
2024-01-09 22:28:54 -0500

    added prettier and eslint configs that work.

--- src/tests/user.test.ts
+++ src/tests/user.test.ts
@@ -2,16 +2,26 @@ import request from "supertest";
 import { User } from "../types/User.js";
 import makeApp from "../app.js";
 import { Express } from "express";
+import { faker } from "@faker-js/faker";
 
 let server: Express = null;
 
 beforeAll((done) => {
-  server = makeApp();
-  done();
+    server = makeApp();
+    done();
 });
-test("/users should return an array of all users", async () => {
-  const response = await request(server).get("/users");
-  const users: User[] = response.body;
-  expect(users.length).toBeGreaterThan(1);
-  console.log(users);
+
+describe("Tests for Creating Users endpoint", () => {
+    test("Creating a users without an email fails", async () => {
+        const response = await request(server)
+            .post("/users/create")
+            .send({ username: "Username", email: "" });
+        expect(response.status).toBe(400);
+    });
+    test("Creating a user without a username fails", async () => {
+        const response = await request(server)
+            .post("/users/create")
+            .send({ username: "", email: "email@gmail.com" });
+        expect(response.status).toBe(400);
+    });
 });
