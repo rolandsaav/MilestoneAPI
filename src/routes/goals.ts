3a9e17eb28949f7f9db8e6f4f767fea28188fcc8
Roland Saavedra <rolandsaavedra22@gmail.com>
2024-02-03 15:27:45 -0500

    Added some comments

--- src/routes/goals.ts
+++ src/routes/goals.ts
@@ -10,60 +10,62 @@ const userCollection = db.collection("users");
 const goalCollection = db.collection("goals");
 
 router.get("/", async (req, res) => {
-  try {
-    const goalsSnapshot = await goalCollection.get();
+    try {
+        const goalsSnapshot = await goalCollection.get();
 
-    let goals: Array<FirebaseFirestore.DocumentData> = [];
-    goalsSnapshot.forEach((goal) => {
-      goals.push(goal.data());
-    });
+        const goals: Array<FirebaseFirestore.DocumentData> = [];
+        goalsSnapshot.forEach((goal) => {
+            goals.push(goal.data());
+        });
 
-    res.json(goals);
-  } catch {}
+        res.json(goals);
+    } catch {
+        /* empty */
+    }
 });
 
 router.post("/:goalId/post/:userId", async (req, res) => {
-  const goalId = req.params.goalId;
-  const userId = req.params.userId;
+    const goalId = req.params.goalId;
+    const userId = req.params.userId;
 
-  const goalDoc = goalCollection.doc(goalId);
-  const goalPromise = await goalDoc.get();
+    const goalDoc = goalCollection.doc(goalId);
+    const goalPromise = await goalDoc.get();
 
-  const userDoc = userCollection.doc(userId);
-  const userPromise = await userDoc.get();
+    const userDoc = userCollection.doc(userId);
+    const userPromise = await userDoc.get();
 
-  const results = await Promise.all([goalPromise, userPromise]);
+    const results = await Promise.all([goalPromise, userPromise]);
 
-  const goalSnapshot = results[0];
-  const userSnapshot = results[1];
+    const goalSnapshot = results[0];
+    const userSnapshot = results[1];
 
-  if (!goalSnapshot.exists) {
-    res.status(400);
-    res.send("No goal with that id exists");
-    return;
-  }
-  if (!userSnapshot.exists) {
-    res.status(400);
-    res.send("No user with that id exists");
-    return;
-  }
+    if (!goalSnapshot.exists) {
+        res.status(400);
+        res.send("No goal with that id exists");
+        return;
+    }
+    if (!userSnapshot.exists) {
+        res.status(400);
+        res.send("No user with that id exists");
+        return;
+    }
 
-  const userData = userSnapshot.data() as User;
-  const goalData = goalSnapshot.data() as Goal;
+    const userData = userSnapshot.data() as User;
+    const goalData = goalSnapshot.data() as Goal;
 
-  const postId = uuidV4();
+    const postId = uuidV4();
 
-  const newPost = {
-    id: postId,
-    creatorId: userData.id,
-    goalId: goalData.id,
-    url: "",
-    date: new Date().toISOString(),
-  };
+    const newPost = {
+        id: postId,
+        creatorId: userData.id,
+        goalId: goalData.id,
+        url: "",
+        date: new Date().toISOString(),
+    };
 
-  const result = await goalDoc.collection("posts").doc(postId).set(newPost);
+    const result = await goalDoc.collection("posts").doc(postId).set(newPost);
 
-  res.send(`New post crated at ${result.writeTime.toDate().toDateString()}`);
+    res.send(`New post crated at ${result.writeTime.toDate().toDateString()}`);
 });
 
 export default router;
